861cde15da07a2d3859bdcf46214f29abd6cf9d3
Sandun-Induranga <sandutharu40@gmail.com>
2022-12-28 00:17:51 +0530

    Content Updated

--- Backend/src/CustomerServlet.java
+++ Backend/src/CustomerServlet.java
@@ -1,4 +1,7 @@
 import javax.annotation.Resource;
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,6 +11,7 @@ import javax.sql.DataSource;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
@@ -23,7 +27,7 @@ public class CustomerServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("Method doGet() Invoked..!");
+
         String cusId = req.getParameter("cusId");
         String cusName = req.getParameter("cusName");
         String cusAddress = req.getParameter("cusAddress");
@@ -37,14 +41,51 @@ public class CustomerServlet extends HttpServlet {
             preparedStatement.setString(1,cusId);
             preparedStatement.setString(2,cusName);
             preparedStatement.setString(3,cusAddress);
-            preparedStatement.setString(4,cusSalary);
+            preparedStatement.setDouble(4,Double.parseDouble(cusSalary));
 
-            preparedStatement.executeUpdate();
+            boolean b = preparedStatement.executeUpdate() > 0;
+            if (b){
+                resp.setStatus(200);
+                resp.getWriter().write("Success");
+            }else {
+                resp.setStatus(500);
+                resp.getWriter().write("Failed");
+            }
 
+            connection.close();
+
+        } catch (SQLException e) {
+
+        }
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            Connection connection = dataSource.getConnection();
+
+            PreparedStatement pstm = connection.prepareStatement("SELECT * FROM Customer");
+            ResultSet resultSet = pstm.executeQuery();
+
+            JsonArrayBuilder allCustomers = Json.createArrayBuilder();
+
+            while (resultSet.next()){
+                JsonObjectBuilder customer = Json.createObjectBuilder();
+                customer.add("id",resultSet.getString(1));
+                customer.add("name",resultSet.getString(2));
+                customer.add("address",resultSet.getString(3));
+                customer.add("salary",resultSet.getString(4));
+
+                allCustomers.add(customer.build());
+            }
+
+            connection.close();
+
+            resp.getWriter().print(allCustomers.build());
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-
     }
 }
